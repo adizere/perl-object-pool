93172e105a9924928164a79783f68458ea108713
root <root@ubuntu.localdomain>
2011-09-16 01:40:47 -0700

    Progress on docs

---
+++ lib/Object/Pool/Element/State/Evicted.pm
@@ -0,0 +1,53 @@
+package Object::Pool::Element::State::Evicted;
+
+use strict;
+use warnings;
+
+use base qw( Object::Pool::Element::State );
+
+
+1; # End of 
+
+__END__
+
+=head1 NAME
+
+
+=head1 SYNOPSIS
+
+
+=head1 EXPORT
+
+
+=head1 METHODS
+
+
+=head1 AUTHOR
+
+Seredinschi Dragos-Adrian, C<< <adizere at gmail.com> >>
+
+
+=head1 BUGS / SUGGESTIONS
+
+If you found any bugs and want to help, or have any suggestions to make, just
+email me.
+
+
+=head1 ACKNOWLEDGEMENTS
+
+Made use of information from article "Pooling", by I<Michael Kircher, Prashant Jain>
+that can be consulted at C<http://www.kircher-schwanninger.de/michael/publications/Pooling.pdf>.
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Seredinschi Dragos-Adrian.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
--- t/00-load.t
+++ t/00-load.t
@@ -1,9 +1,21 @@
-#!perl -T
-
-use Test::More tests => 1;
+use Test::More tests => 8;
 
 BEGIN {
-    use_ok( 'Object::Pool' ) || print "Bail out!\n";
+
+    my @class_names = (
+        'Object::Pool',
+        'Object::PoolFactory',
+        'Object::Pool::Element',
+        'Object::Pool::Element::State',
+        'Object::Pool::Element::State::Busy',
+        'Object::Pool::Element::State::Free',
+        'Object::Pool::Element::State::New',
+        'Object::Pool::Element::State::Evicted'
+    );
+
+    foreach( @class_names ) { 
+        use_ok( $_ ) || print "Bailing out at $_ !\n";
+    }
 }
 
-diag( "Testing Object::Pool $Object::Pool::VERSION, Perl $], $^X" );
+diag( "Testing Object::Pool $Object::Pool::VERSION classes, Perl $], $^X" );
